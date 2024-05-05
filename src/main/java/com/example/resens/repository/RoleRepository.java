7096d27532ee5cd6fc1cc1193ed595c6b4127c54
hamzamekni <hamzamekni4@gmail.com>
2024-05-05 01:19:51 +0100

    adding services and repos and impl for every model

--- src/main/java/com/example/resens/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.resens.repository;
-
-import com.example.resens.enumeration.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
